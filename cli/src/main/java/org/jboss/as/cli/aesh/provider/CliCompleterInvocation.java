9512e3fa85551db440272652fba6740d3ea8ba96
Jean-Francois Denise <jfdenise@gmail.com>
2016-05-19 14:50:41 +0200

    adding providers, where missing from first commit

---
+++ cli/src/main/java/org/jboss/as/cli/aesh/provider/CliCompleterInvocation.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.as.cli.aesh.provider;
+
+import java.util.Collection;
+import org.jboss.aesh.console.AeshContext;
+import org.jboss.aesh.console.command.Command;
+import org.jboss.aesh.console.command.completer.CompleterInvocation;
+import org.jboss.aesh.terminal.TerminalString;
+import org.jboss.as.cli.CommandContext;
+
+import java.util.List;
+import org.jboss.aesh.console.command.registry.CommandRegistry;
+
+/**
+ * @author <a href="mailto:stale.pedersen@jboss.org">Ståle W. Pedersen</a>
+ */
+public class CliCompleterInvocation implements CompleterInvocation {
+
+    private final CompleterInvocation delegate;
+    private final CommandContext commandContext;
+    private final CommandRegistry registry;
+
+    public CliCompleterInvocation(CompleterInvocation delegate, CommandContext ctx,
+            CommandRegistry registry) {
+        this.delegate = delegate;
+        this.commandContext = ctx;
+        this.registry = registry;
+    }
+
+    public CommandRegistry getCommandRegistry() {
+        return registry;
+    }
+
+    @Override
+    public String getGivenCompleteValue() {
+        return delegate.getGivenCompleteValue();
+    }
+
+    @Override
+    public Command getCommand() {
+        return delegate.getCommand();
+    }
+
+    @Override
+    public List<TerminalString> getCompleterValues() {
+        return delegate.getCompleterValues();
+    }
+
+    @Override
+    public void setCompleterValuesTerminalString(List<TerminalString> terminalStrings) {
+        delegate.setCompleterValuesTerminalString(terminalStrings);
+    }
+
+    @Override
+    public void clearCompleterValues() {
+        delegate.clearCompleterValues();
+    }
+
+    @Override
+    public void addCompleterValue(String s) {
+        delegate.addCompleterValue(s);
+    }
+
+    @Override
+    public void addCompleterValueTerminalString(TerminalString terminalString) {
+        delegate.addCompleterValueTerminalString(terminalString);
+    }
+
+    @Override
+    public boolean isAppendSpace() {
+        return delegate.isAppendSpace();
+    }
+
+    @Override
+    public void setAppendSpace(boolean b) {
+        delegate.setAppendSpace(b);
+    }
+
+    @Override
+    public void setIgnoreOffset(boolean ignoreOffset) {
+        delegate.setIgnoreOffset(ignoreOffset);
+    }
+
+    @Override
+    public boolean doIgnoreOffset() {
+        return delegate.doIgnoreOffset();
+    }
+
+    @Override
+    public void setOffset(int offset) {
+        delegate.setOffset(offset);
+    }
+
+    @Override
+    public int getOffset() {
+        return delegate.getOffset();
+    }
+
+    @Override
+    public void setIgnoreStartsWith(boolean ignoreStartsWith) {
+        delegate.setIgnoreStartsWith(ignoreStartsWith);
+    }
+
+    @Override
+    public boolean isIgnoreStartsWith() {
+        return delegate.isIgnoreStartsWith();
+    }
+
+    @Override
+    public AeshContext getAeshContext() {
+        return delegate.getAeshContext();
+    }
+
+    public CommandContext getCommandContext() {
+        return commandContext;
+    }
+
+    @Override
+    public void setCompleterValues(Collection<String> completerValues) {
+        delegate.setCompleterValues(completerValues);
+    }
+
+    @Override
+    public void addAllCompleterValues(Collection<String> completerValues) {
+        delegate.addAllCompleterValues(completerValues);
+    }
+}
