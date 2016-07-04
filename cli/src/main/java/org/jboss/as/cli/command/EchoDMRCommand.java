038f174c9b48699ef2acf244c59f845f90db5baf
Jean-Francois Denise <jfdenise@gmail.com>
2016-07-04 19:52:27 +0200

    echo-dmr can't be replaced, keep the bridge

---
+++ cli/src/main/java/org/jboss/as/cli/command/EchoDMRCommand.java
@@ -0,0 +1,91 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2016, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.cli.command;
+
+import org.jboss.aesh.cl.CommandDefinition;
+import org.jboss.aesh.console.command.Command;
+import org.jboss.aesh.console.command.CommandResult;
+import java.util.List;
+import org.jboss.aesh.cl.Arguments;
+import org.jboss.aesh.console.command.CommandException;
+import org.jboss.aesh.console.command.CommandNotFoundException;
+import org.jboss.as.cli.CommandFormatException;
+import org.jboss.as.cli.console.CliCommandRegistry;
+import org.jboss.dmr.ModelNode;
+import org.wildfly.core.cli.command.CliCommandInvocation;
+import org.wildfly.core.cli.command.DMRCommand;
+
+/**
+ * A Command to echo variables. This is not activated, we are missing a proper
+ * completer and options handling, eg: echo-dmr ls -l is badly handled.
+ * *
+ * @author jdenise@redhat.com
+ */
+@CommandDefinition(name = "echo-dmr", description = "")
+public class EchoDMRCommand implements Command<CliCommandInvocation> {
+
+    // XXX JFDENISE, NEED A COMPLETER FOR COMMAND.
+    @Arguments()
+    private List<String> cmd;
+
+    @Override
+    public CommandResult execute(CliCommandInvocation commandInvocation)
+            throws CommandException, InterruptedException {
+        if (cmd != null && cmd.size() > 0) {
+            try {
+                echoDMR(commandInvocation);
+            } catch (CommandFormatException | CommandNotFoundException ex) {
+                throw new CommandException(ex);
+            }
+        } else {
+            commandInvocation.println("Missing the command or operation to translate to DMR.");
+        }
+        return null;
+    }
+
+    private void echoDMR(CliCommandInvocation commandInvocation)
+            throws CommandException, CommandNotFoundException,
+            InterruptedException, CommandFormatException {
+        StringBuilder builder = new StringBuilder();
+        for (String s : cmd) {
+            builder.append(s).append(" ");
+        }
+        commandInvocation.println(retrieveRequest(cmd.get(0), builder.toString(),
+                commandInvocation).toString());
+    }
+
+    private ModelNode retrieveRequest(String opName, String originalInput,
+            CliCommandInvocation commandInvocation)
+            throws CommandNotFoundException, InterruptedException, CommandException, CommandFormatException {
+        final Command handler = ((CliCommandRegistry) commandInvocation.getCommandRegistry()).
+                findCommand(opName, originalInput);
+        if (handler == null) {
+            throw new CommandException("No command handler for '" + opName + "'.");
+        }
+
+        if (!(handler instanceof DMRCommand)) {
+            throw new CommandException("The command does not translate to an operation request.");
+        }
+
+        return ((DMRCommand) handler).buildRequest(originalInput, commandInvocation.getCommandContext());
+    }
+}
