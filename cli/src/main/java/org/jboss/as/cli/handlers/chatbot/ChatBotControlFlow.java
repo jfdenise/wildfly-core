dbb5c4ea1ebca7d4eafe48cb04525041c5f420ba
Jean-Francois Denise <jdenise@redhat.com>
2024-02-29 14:54:55 +0100

    Hack the dependencies when building core feature-pack

--- cli/src/main/java/org/jboss/as/cli/handlers/chatbot/ChatBotControlFlow.java
+++ cli/src/main/java/org/jboss/as/cli/handlers/chatbot/ChatBotControlFlow.java
@@ -36,6 +36,7 @@ import org.jboss.as.cli.operation.ParsedCommandLine;
 public class ChatBotControlFlow implements CommandLineRedirection {
 
     private static final String CTX_KEY = "CHATBOT";
+    private static final String CHAIN_KEY = "CHAIN";
 
     private ConversationalRetrievalChain chain;
 
@@ -47,46 +48,51 @@ public class ChatBotControlFlow implements CommandLineRedirection {
 
     ChatBotControlFlow(CommandContext ctx) throws CommandLineException {
         checkNotNullParam("ctx", ctx);
-        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
-        EmbeddingStore<TextSegment> store = loadEmbeddingStore(
-                new File("").toPath().resolve("docs-wildfly-embedding.json"));
-        ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
-                .embeddingStore(store)
-                .embeddingModel(embeddingModel)
-                .maxResults(2) // on each interaction we will retrieve the 2 most relevant segments
-                .minScore(0.5) // we want to retrieve segments at least somewhat similar to user query
-                .build();
-        ChatLanguageModel model = OpenAiChatModel
-                .builder()
-                .apiKey("demo")
-                .maxRetries(5)
-                .modelName(OpenAiChatModelName.GPT_3_5_TURBO)
-                .logRequests(Boolean.TRUE)
-                .logResponses(Boolean.TRUE)
-                .maxTokens(1000)
-                .build();
-        String question = "What are the CLI commands to enable logging?";
-        String promptTemplate2 = "You are a chatbot that will provide assistance with questions about WildFly.\n"
-                + "You will be given a question you need to answer and a context to provide you with information.\n"
-                + "You must answer the question based as much as possible on this context.\n"
-                + "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct.\n"
-                + "If you don't know the answer to a question, please don't share false information. Only reply with CLI commands. Answer the user question delimited by  ---."
-                + "\n"
-                + "---\n"
-                + "{{userMessage}}\n"
-                + "---"
-                + "\n Here is a few data to help you:\n"
-                + "{{contents}}";
+        chain = (ConversationalRetrievalChain) ctx.get(Scope.CONTEXT, CHAIN_KEY);
+        if (chain == null) {
+            EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+            EmbeddingStore<TextSegment> store = loadEmbeddingStore(
+                    new File("").toPath().resolve("docs-wildfly-embedding.json"));
+            ContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+                    .embeddingStore(store)
+                    .embeddingModel(embeddingModel)
+                    .maxResults(2) // on each interaction we will retrieve the 2 most relevant segments
+                    .minScore(0.5) // we want to retrieve segments at least somewhat similar to user query
+                    .build();
+            ChatLanguageModel model = OpenAiChatModel
+                    .builder()
+                    .apiKey("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")
+                    .maxRetries(5)
+                    .modelName(OpenAiChatModelName.GPT_3_5_TURBO)
+                    .logRequests(Boolean.TRUE)
+                    .logResponses(Boolean.TRUE)
+                    .maxTokens(1000)
+                    .build();
+            String question = "What are the CLI commands to enable logging?";
+            String promptTemplate2 = "You are a chatbot that will provide assistance with questions about WildFly.\n"
+                    + "You will be given a question you need to answer and a context to provide you with information.\n"
+                    + "You must answer the question based as much as possible on this context.\n"
+                    + "If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct.\n"
+                    + "If you don't know the answer to a question, please don't share false information. Only reply with CLI commands. Answer the user question delimited by  ---."
+                    + "\n"
+                    + "---\n"
+                    + "{{userMessage}}\n"
+                    + "---"
+                    + "\n Here is a few data to help you:\n"
+                    + "{{contents}}";
 
-        chain = ConversationalRetrievalChain.builder()
-                .chatLanguageModel(model)
-                .retrievalAugmentor(DefaultRetrievalAugmentor.builder()
-                        .contentInjector(DefaultContentInjector.builder()
-                                .promptTemplate(PromptTemplate.from(promptTemplate2))
-                                .build())
-                        .queryRouter(new DefaultQueryRouter(contentRetriever))
-                        .build())
-                .build();
+            chain = ConversationalRetrievalChain.builder()
+                    .chatLanguageModel(model)
+                    .retrievalAugmentor(DefaultRetrievalAugmentor.builder()
+                            .contentInjector(DefaultContentInjector.builder()
+                                    .promptTemplate(PromptTemplate.from(promptTemplate2))
+                                    .build())
+                            .queryRouter(new DefaultQueryRouter(contentRetriever))
+                            .build())
+                    .build();
+            ctx.set(Scope.CONTEXT, CTX_KEY, chain);
+        }
+        ctx.printLine("Hello, I am the WildFly chatbot, ready to help you setup your WildFly server. Type your questions. Type 'bye' to leave.");
         ctx.set(Scope.CONTEXT, CTX_KEY, this);
     }
 
